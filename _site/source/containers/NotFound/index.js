8eef8500abc9a2b99300c51fff4cc351f9c6d7a0
Liam Butterworth <wbbutterworth@gmail.com>
2018-07-12 08:59:49 -0500

    remove redirect code from index and 404; add test message to not found index

--- _site/source/containers/NotFound/index.js
+++ _site/source/containers/NotFound/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => (
 	<div>
 		<h1>404 - Not Found</h1>
-		<p>Sorry, but the page you are looking for does not exist.</p>
+		<p>Sorry, but the page you are looking for does not exist. TEST</p>
 	</div>
 );
 
