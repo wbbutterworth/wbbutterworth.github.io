5c6bf5817b7a1073f816b5497f4df6c184c9fbfa
Liam Butterworth <account@liambutterworth.me>
2018-07-22 22:31:07 -0500

    add flexbox to footer; refactor styles for consistency and use font-size mixin where relevant

--- _site/src/components/Footer/index.js
+++ _site/src/components/Footer/index.js
@@ -9,13 +9,15 @@ require( './style.css' );
 const Footer = () => (
 	<footer id="contact" className="footer">
 		<div className="footer-inner">
-			<h1 className="footer-text">Say <a href="mailto:hello@liambutterworth.me">hello@liambutterworth.me</a></h1>
+			<section>
+				<h1 className="footer-text">Say <a href="mailto:hello@liambutterworth.me">hello@liambutterworth.me</a></h1>
 
-			<div className="footer-links">
-				<a href="/liambutterworth.pdf" target="_blank" rel="noopener noreferrer"><Icon svg={ fileSVG } /></a>
-				<a href="https://github.com/liambutterworth" target="_blank" rel="noopener noreferrer"><Icon svg={ githubSVG } /></a>
-				<a href="https://linkedin.com/in/liam-butterworth" target="_blank" rel="noopener noreferrer"><Icon svg={ linkedinSvg } /></a>
-			</div>
+				<div className="footer-links">
+					<a href="/liambutterworth.pdf" target="_blank" rel="noopener noreferrer"><Icon svg={ fileSVG } /></a>
+					<a href="https://github.com/liambutterworth" target="_blank" rel="noopener noreferrer"><Icon svg={ githubSVG } /></a>
+					<a href="https://linkedin.com/in/liam-butterworth" target="_blank" rel="noopener noreferrer"><Icon svg={ linkedinSvg } /></a>
+				</div>
+			</section>
 		</div>
 	</footer>
 );
